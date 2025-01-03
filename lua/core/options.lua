f718c769aa674b4e6b245ee5b06b753753155a75
takibist-No9 <yasuyuki.chinen@gmail.com>
2025-01-03 18:33:10 +0900

    initial commit

---
+++ lua/core/options.lua
@@ -0,0 +1,18 @@
+vim.wo.number = true -- Make line numbers default
+vim.o.relativenumber = true -- Set relative numberd lines
+vim.o.clipboard = 'unnamedplus' -- Sync clipboard between OS and Neovim.
+vim.o.wrap = false -- Display lines as one long line
+vim.o.linebreak = true -- Companion to wrap, don't split words
+vim.o.mouse = '' -- Unable mouse mode
+
+vim.o.autoindent = true -- Copy indenet from current line when starting new one
+vim.o.ignorecase = true -- Case-insensitive seraching UNLESS \C or capital in search
+vim.o.smartcase = true -- Smart case
+vim.o.tabstop = 2 -- Show tab as two spaces
+vim.o.shiftwidth = 2 -- Indent using two columns
+vim.o.expandtab = true -- Uses spaces instead of tabs
+
+vim.o.splitbelow = true -- Create new horizontal widows below
+vim.o.splitright = true -- Create new vertical windows to the right
+vim.o.hlsearch = true -- Highlight search
+vim.o.incsearch = true -- Incremental search
---
+++ lua/plugins/toggleterm.lua
@@ -0,0 +1,9 @@
+return {
+  "akinsho/toggleterm.nvim",
+  version = "*",
+  opts = {
+    direction = "float",
+    open_mapping = [[<c-t>]],
+  },
+  config = true,
+}
